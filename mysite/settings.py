8f89e7cff0cee4499cf20e8a98843bd521971205
mohamed hawas <mohamedhawas123@gmail.com>
2021-09-13 02:00:20 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!p1ai_b373g3bmz-**m@%h9+0_8xm7*41etdbi+t266-mogm08'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'blogysaif.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'blogger-my.herokuapp.com']
 
 if DEBUG:
     # During development only
